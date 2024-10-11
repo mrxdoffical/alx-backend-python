e94cd9bc5dbdfb0117879985ea803526a4da1f2b
mrxdoffical <berogergesg@gmail.com>
2024-10-11 21:13:14 +0300

    task4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env pythno3
+"""
+this module i don't know what it does nut it's something with variables
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
