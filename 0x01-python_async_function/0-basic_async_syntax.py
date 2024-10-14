9bcbf7ddeb7cc76cd570474bedc097dd613a96b1
mrxdoffical <berogergesg@gmail.com>
2024-10-14 22:52:23 +0300

    task0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import asyncio
-import random
-
 """
 This module waits for a random time to pass.
 """
+
+
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
