c43f8811065e7b006ca4876c04eba7d2ae17c234
mrxdoffical <berogergesg@gmail.com>
2024-10-15 17:27:50 +0300

    task3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,11 +5,11 @@ this module do something with tasks
 
 
 import asyncio
-from typing import Any
+from typing import Any, Union
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns an asyncio.Task for the wait_random coroutine.
 
