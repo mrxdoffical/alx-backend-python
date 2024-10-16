839044f294d897588ba72043287abd063396a40f
mrxdoffical <berogergesg@gmail.com>
2024-10-16 21:32:28 +0300

    task0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,9 +9,9 @@ import random
 
 
 async def async_generator():
-	"""
+    """
     An asynchronous generator that yields a random float between 0 and 10.
-	"""
-	for hellYah in range(10):
-		await asyncio.sleep(1)
-		yield random.uniform(0, 10)
+    """
+    for hellYah in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
