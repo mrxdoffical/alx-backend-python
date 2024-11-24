a0b4f55a4be4402b9f26d8f739555d2d7bbf08ba
mrxdoffical <berogergesg@gmail.com>
2024-11-24 23:07:06 +0200

    task5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """A module for testing the client module.
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -22,6 +22,27 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{org_name}"
         )
+
+    def test_public_repos_url(self):
+        """Test that the result of _public_repos_url is the expected one
+        based on the mocked payload.
+        """
+        test_payload = {
+            'repos_url': "https://api.github.com/orgs/google/repos"
+        }
+
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock,
+            return_value=test_payload
+        ) as mock_org:
+            test_client = GithubOrgClient("google")
+            result = test_client._public_repos_url
+            self.assertEqual(
+                result,
+                test_payload["repos_url"]
+            )
+            mock_org.assert_called_once()
 
 
 if __name__ == '__main__':
